7c683b8859dfcb6bf282886a84c8b15a15b4f7ba
geekyjha9 <geekyjha@gmail.com>
2023-07-22 17:29:50 +0530

    somthings

---
+++ README.md
@@ -0,0 +1,13 @@
+# Namaste React 👨‍💻
+
+# Parcel
+    - Dev Build
+    - Local Server
+    - HMR - Hod Module Replacement
+    - File Watching Algorithm - written in C++
+    - Cashing - Faster Builds
+    - Image Optimization 
+    - Minification
+    - Bundling
+    - Compress
+    - 
--- dist/index.6bd02f5a.js
+++ dist/index.6bd02f5a.js
@@ -1,32 +1,30 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-
 // const heading = React.createElement(
 //   "h1",
 //   { id: "heading", xyz: "abc"},
 //   "Hello World From React!"
 // );
-
 // It is a React Element or Javascript object
-
 // Concept of nested Divs
-
-const heading = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "div1" },
-    React.createElement("div", { id: "div2" }, [
-      React.createElement("h1", { id: "h1" }, "Hello World!"),
-      React.createElement("h1", { id: "h1" }, "Shivam Jha"),
-      React.createElement("h1", { id: "h1" }, "Ankit Singh"),
-    ])
-  )
-);
-
+const heading = React.createElement("div", {
+    id: "parent"
+}, React.createElement("div", {
+    id: "div1"
+}, React.createElement("div", {
+    id: "div2"
+}, [
+    React.createElement("h1", {
+        id: "h1"
+    }, "Hello World!"),
+    React.createElement("h1", {
+        id: "h1"
+    }, "Hello World!"),
+    React.createElement("h1", {
+        id: "h1"
+    }, "Hello World!")
+])));
 console.log(heading); // object
 console.log(typeof heading);
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading); // render method converts react element or javascript object into html element & put it into provided location to render method.
+
+//# sourceMappingURL=index.6bd02f5a.js.map
