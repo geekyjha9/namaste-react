9a479d135b83139c45fa829c098fa760a7b9d1bd
geekyjha9 <geekyjha@gmail.com>
2023-07-22 14:24:58 +0530

    committed

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading", xyz: "abc"},
+//   "Hello World From React!"
+// );
+
+// It is a React Element or Javascript object
+
+
+// Concept of nested Divs
+
 const heading = React.createElement(
-  "h1",
-  { id: "heading", xyz: "abc"},
-  "Hello World From React!"
-); // It is React Element or Javascript Object
+  "div",
+  { id: "parent" },
+  React.createElement(  
+    "div",
+    { id: "div1" }, 
+    React.createElement(
+      "div",
+      { id: "div2" },
+      React.createElement("div", { id: "div3"}, "Hello World!")
+    )
+  )
+);
 
 console.log(heading); // object
-console.log(typeof(heading));
+console.log(typeof heading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading); // render method converts react element or javascript object into html element & put it into provided location to render method. 
-
+root.render(heading); // render method converts react element or javascript object into html element & put it into provided location to render method.
